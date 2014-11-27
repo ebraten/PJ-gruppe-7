f841c98956bce1157d450b5f17f82bd76c8765b9
Aleksander Espenakk <espale13@student.westerdals.no>
2014-11-27 13:43:30 +0100

    update to bevegelse og bygning

--- Bygning.cs
+++ Bygning.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class Bygning : MonoBehaviour {
 
-	void OnCollisionEnter2D (Collision2D col)
+	void OnTriggerEnter2D (Collider2D col)
 	{
-			Destroy(this.gameObject);
+			Application.LoadLevel(tag);
 	}
 }
