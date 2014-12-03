51b4aa9cad96ddbf45667ee885bf20924556811a
Alkanar <alkanargj@gmail.com>
2014-12-03 15:02:24 +0100

    Commented code

---
+++ snakking.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class snakking : MonoBehaviour {
+
+	private bool popup;
+
+	void OnTriggerEnter2d(Collision2D col){
+		popup = true;
+	}
+
+	void DrawInfo()
+	{
+		Rect close = new Rect (250,50,50,50);
+		if (popup)
+		{
+			GUI.Box(new Rect(100, 50, 100, 50), "hey");
+			if (GUI.Button (close,"X"))
+			{
+				popup = false;
+			}
+		}
+	}
+
+	void OnGUI()
+	{
+		DrawInfo();
+	}
+}
