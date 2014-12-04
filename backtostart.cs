a01a2db26f2ed21e6975fafc78d0ec7db21fa61d
Alkanar <alkanargj@gmail.com>
2014-12-04 15:47:09 +0100

    Removed old files

--- backtostart.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class backtostart : MonoBehaviour {
-	void OnMouseDown () {
-		//Loads Application on click
-		Application.LoadLevel(0);
-	}
-}
