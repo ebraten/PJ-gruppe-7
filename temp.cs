65e9ec337a9cf27f475ec29ace0bdbda759a8aad
Alkanar <alkanargj@gmail.com>
2014-12-04 15:28:57 +0100

    Added level unlocking

---
+++ temp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class temp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI () {
+		if (GUI.Button (new Rect (0,0,75,25), "Reset")) {
+				//Reset game
+				PlayerPrefs.DeleteAll();
+				Application.LoadLevel(0);
+			}
+	}
+}
