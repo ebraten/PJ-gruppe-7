88ede545d58caeac1ef7e42b5d823b2fdf1b15ac
espale13 <espale13@student.westerdals.no>
2015-02-28 17:53:44 +0100

    Getting the magnefying glas

---
+++ GettingMouse.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GettingMouse : MonoBehaviour {
+
+	public static bool mouse = false;
+	public Texture2D cursorImage;
+	private int cursorWidth = 70;
+	private int cursorHeight = 70;
+
+	void OnGUI()
+	{
+		if (mouse)
+		{
+			Screen.showCursor = false;
+			
+			GUI.DrawTexture (new Rect (Input.mousePosition.x - 20, Screen.height - Input.mousePosition.y - 20, cursorWidth, cursorHeight), cursorImage);
+		}
+	}
+}
---
+++ destroyforstør.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyforstør : MonoBehaviour {
+	
+	void OnMouseDown()
+	{
+		GettingMouse.mouse = true;
+		Destroy(gameObject);
+	}
+}
