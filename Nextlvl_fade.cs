e3e4e5e453cab2167230cc0f37b6d6a40d9fcfb3
snifty <aleksanderne1@gmail.com>
2015-03-10 14:18:57 +0100

    Fade between lvls
    not working propperly

---
+++ Nextlvl_fade.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nextlvl_fade : MonoBehaviour {
+	public int unlockedAfter;
+	public bool clickToGo = false;
+	
+	
+	void OnMouseDown() {
+		StartCoroutine (myCoroutine());
+	}
+	
+	IEnumerator myCoroutine () {
+		float fadeTime = GameObject.Find("Main Camera").GetComponent<fade>().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		goNextLvl ();
+	}
+	
+	void goNextLvl(){
+		Application.LoadLevel (tag);
+	}
+}
---
+++ fade.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fade : MonoBehaviour {
+
+	public Texture2D fadeOutTexture;
+	public float fadespeed = 0.8f;
+
+	private int drawDepth = 1;
+	private float alpha = 1.0f;
+	private int fadeDir = 1;
+
+	//funker ikke helt
+
+	void OnGui(){
+		alpha += fadeDir * fadespeed * Time.deltaTime;
+		alpha = Mathf.Clamp01 (alpha);
+
+		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+		GUI.depth = drawDepth;
+		GUI.DrawTexture (new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+	}
+
+	public float BeginFade (int direction){
+		fadeDir = direction;
+		return (fadespeed);
+	}
+
+	void OnLevelWasLoaded () {
+		BeginFade (-1);
+	}
+}
